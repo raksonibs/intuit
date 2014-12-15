96b00b1f7222770a256a186c7c1555c980ebac92
Oskar Niburski <oskarniburski@gmail.com>
2014-12-15 10:09:12 -0500

    Update to initializers

---
+++ intuit/config/initializers/quickbooks.rb
@@ -0,0 +1,8 @@
+$qb_oauth_consumer = OAuth::Consumer.new(Figaro.env.qb_key, Figaro.env.qb_secret, {
+    :site                 => "https://oauth.intuit.com",
+    :request_token_path   => "/oauth/v1/get_request_token",
+    :authorize_url        => "https://appcenter.intuit.com/Connect/Begin",
+    :access_token_path    => "/oauth/v1/get_access_token"
+})
+
+Quickbooks.sandbox_mode = true
