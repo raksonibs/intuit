b7a01678abac43ea8eba1b4b108bdb2a51fd664d
Oskar Niburski <oskarniburski@gmail.com>
2014-12-15 15:00:31 -0500

    Update to vendor to use company as model company reference

--- intuit/app/controllers/vendors_controller.rb
+++ intuit/app/controllers/vendors_controller.rb
@@ -1,6 +1,5 @@
 class VendorsController < ApplicationController
   before_action :set_vendor, only: [:show, :edit, :update, :destroy]
-  before_action :set_qb_service, only: [:create, :edit, :update, :destroy]
 
   # GET /vendors
   # GET /vendors.json
@@ -20,21 +19,6 @@ class VendorsController < ApplicationController
 
   # GET /vendors/1/edit
   def edit
-  end
-
-  def authenticate
-    callback = oauth_callback_vendors_url
-    token = $qb_oauth_consumer.get_request_token(:oauth_callback => callback)
-    session[:qb_request_token] = Marshal.dump(token)
-    redirect_to("https://appcenter.intuit.com/Connect/Begin?oauth_token=#{token.token}") and return
-  end
-
-  def oauth_callback
-    at = Marshal.load(session[:qb_request_token]).get_access_token(:oauth_verifier => params[:oauth_verifier])
-    session[:token] = at.token
-    session[:secret] = at.secret
-    session[:realm_id] = params['realmId']
-    redirect_to root_url, notice: "Your QuickBooks account has been successfully linked."
   end
 
   # POST /vendors
@@ -92,22 +76,8 @@ class VendorsController < ApplicationController
     end
   end
 
-  def logout
-    session[:qb_request_token] = nil
-    session[:token] = nil
-    session[:secret] = nil
-    session[:realm_id] = nil
-    redirect_to root_url
-  end
-
   private
 
-    def set_qb_service
-      oauth_client = OAuth::AccessToken.new($qb_oauth_consumer, session[:token], session[:secret])
-      @vendor_service = Quickbooks::Service::Vendor.new
-      @vendor_service.access_token = oauth_client
-      @vendor_service.company_id = session[:realm_id]
-    end
     # Use callbacks to share common setup or constraints between actions.
     def set_vendor
       @vendor = Vendor.find(params[:id])
--- intuit/config/routes.rb
+++ intuit/config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
-  resources :vendors do 
+  get 'companies/create'
+
+  get 'companies/authenticate'
+
+  get 'companies/logout'
+
+  get 'companies/oauth_callback'
+
+  resources :vendors
+
+  root to: 'companies#index'
+
+  resources :companies do 
     collection do 
       get :authenticate
       get :oauth_callback
+      get :logout
     end
   end
-
-  get 'logout', to: 'vendors#logout', as: :logout
-
-  root to: 'vendors#index'
 
 end
