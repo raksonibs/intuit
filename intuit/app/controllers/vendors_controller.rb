8b213e315a7a8a2261d66a5e6fe0487fbee8bb8b
Oskar Niburski <oskarniburski@gmail.com>
2014-12-15 12:00:02 -0500

    Vendor controller update

--- intuit/app/controllers/vendors_controller.rb
+++ intuit/app/controllers/vendors_controller.rb
@@ -1,5 +1,6 @@
 class VendorsController < ApplicationController
   before_action :set_vendor, only: [:show, :edit, :update, :destroy]
+  before_action :set_qb_service, only: [:create, :edit, :update, :destroy]
 
   # GET /vendors
   # GET /vendors.json
@@ -41,6 +42,11 @@ class VendorsController < ApplicationController
   def create
     @vendor = Vendor.new(vendor_params)
 
+    vendor = Quickbooks::Model::Vendor.new 
+    vendor.given_name = vendor_params[:name]
+    vendor.email_address = vendor_params[:email_address]
+    @vendor_service.create(vendor)
+
     respond_to do |format|
       if @vendor.save
         format.html { redirect_to @vendor, notice: 'Vendor was successfully created.' }
@@ -55,8 +61,18 @@ class VendorsController < ApplicationController
   # PATCH/PUT /vendors/1
   # PATCH/PUT /vendors/1.json
   def update
+    Quickbooks.logger = Rails.logger 
+    Quickbooks.log = true 
+    
     respond_to do |format|
       if @vendor.update(vendor_params)
+        #this line checkts over the vendor service, checking all entriies. where is @vendor_service readily available though?
+        vendor = @vendor_service.query("SELECT * FROM VENDOR WHERE GivenName = '#{vendor_params[:name]}'").entries.first
+
+        vendor.email_address = vendor_params[:email_address]
+
+        @vendor_service.update(vendor)
+
         format.html { redirect_to @vendor, notice: 'Vendor was successfully updated.' }
         format.json { render :show, status: :ok, location: @vendor }
       else
@@ -77,6 +93,13 @@ class VendorsController < ApplicationController
   end
 
   private
+
+    def set_qb_service
+      oauth_client = OAuth::AccessToken.new($qb_oauth_consumer, session[:token], session[:secret])
+      @vendor_service = Quickbooks::Service::Vendor.new
+      @vendor_service.access_token = oauth_client
+      @vendor_service.company_id = session[:realm_id]
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_vendor
       @vendor = Vendor.find(params[:id])
@@ -84,6 +107,6 @@ class VendorsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def vendor_params
-      params.require(:vendor).permit(:name)
+      params.require(:vendor).permit(:name, :email_address)
     end
 end
